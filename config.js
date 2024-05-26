67ad483667674356b217a3a527fb44734bb90d01
nandish-yadav <nanduyadavrny@gmail.com>
2024-05-26 14:40:40 +0530

    basic project setup

---
+++ config.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+
+// Load environment variables from the .env file
+dotenv.config();
+
+
+// Define the configuration with fallback defaults
+const config = {
+    port: parseInt(process.env.PORT || '3000'),
+    mongoUri: process.env.MONGODB_URI || 'mongodb+srv://nanduyadavrny:lZtRzqLpTkdDQRMO@cluster0.mhq2v9n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    jwtSecret: process.env.JWT_SECRET || 'your_secret_key',
+    environment: process.env.NODE_ENV || 'development'
+};
+
+module.exports =  config;
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const config = require('./config');
+
+const PORT = config.port || 3000;
+const app = express();
+dotenv.config();
+
+
+
+// Middlewares
+app.use(cors());                // Enables CORS
+app.use(express.json());        // Parses incoming JSON requests and puts the parsed data in req.body
+
+// Database connection
+const mongoUri = config.mongoUri;
+mongoose.connect(mongoUri, {
+    autoIndex: true,
+}).then(() => console.log('MongoDB connected successfully.'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World! test route');
+});
+
+
+
+// Listen on provided port, on all network interfaces
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log('Press CTRL-C to stop\n');
+});
+
+// Handle unhandled promise rejections (e.g., MongoDB connection issues)
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    // Application specific logging, throwing an error, or other logic here
+  });
+
+  // Handle uncaught exceptions
+process.on('uncaughtException', (error) => {
+    console.error('Uncaught Exception:', error);
+    process.exit(1); // Mandatory (as per the Node.js docs)
+  });
